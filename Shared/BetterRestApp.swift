3b9814f8a3171f7ad63fd090e6d1119df77dd179
avonderg <77714730+avonderg@users.noreply.github.com>
2022-01-13 17:41:11 -0500

    Initial Commit

---
+++ Shared/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  Shared
+//
+//  Created by Alexandra von der Goltz on 1/13/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
